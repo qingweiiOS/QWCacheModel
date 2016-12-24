40120eb599b3b5549d947f0b09233262df82396d
qingweiiOS <772053795@qq.com>
2016-12-24 14:53:20 +0800

    Initial Commit

---
+++ QWCacheModel/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  QWCacheModel
+//
+//  Created by qingweiqw on 16/12/24.
+//  Copyright © 2016年 qingweiqw. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ QWCacheModel/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  QWCacheModel
+//
+//  Created by qingweiqw on 16/12/24.
+//  Copyright © 2016年 qingweiqw. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
